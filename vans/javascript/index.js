9bd9cb365305486052d7a3a3dad2d916fa024a73
김가현 <gahyun3722@gmail.com>
2024-03-06 10:00:27 +0900

    1차

---
+++ vans/javascript/index.js
@@ -0,0 +1,11 @@
+$('li.main-menu').mouseenter(function(){
+    $('li.main-menu').removeClass('on')
+    $(this).addClass('on')
+})
+
+$('li.main-menu').mouseleave(function(){
+    $('li.main-menu').removeClass('on')
+    
+})
+
+
